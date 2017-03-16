2cc0fb1b66b18dab25a596d7a7dd0ab4675c78c4
vivek <vivekvenkris@gmail.com>
2017-03-16 18:50:01 +1100

    Added python dir. Please put all python here.

---
+++ src/examples/JepTest.java
@@ -0,0 +1,11 @@
+package examples;
+
+import jep.Jep;
+import jep.JepException;
+
+public class JepTest {
+	public static void main(String[] args) throws JepException {
+		Jep jep = new Jep(false);
+		
+	}
+}
